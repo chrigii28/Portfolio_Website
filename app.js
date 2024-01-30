b17fa63bc6229cb502ad16a813f347757a277f0f
Kristian Velkov <velkov.kristian@gmail.com>
2024-01-31 00:11:23 +0100

    IchDreihDure

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+      return {
+          imageSources: [
+          "https://drive.google.com/thumbnail?id=1JNEuMBQkj2WerZUXLGNXTbO-y-pTJ-Gs&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1zDZJwIB4whY194Rj9OAVt_6dqmOp7pm8&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1bmo5nBykgPJOhap-7YWxjlUS5knljQuE&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1sMf_vgsmQAJGpu6ih2EXNWw_aN1IyKUY&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1Wqk8IPCkE2J8cYWPB9Ni2ENS3x7OyhgV&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1fDHBvj75wxSovyZBf7GGrPQzhVvfdYr1&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1fDHBvj75wxSovyZBf7GGrPQzhVvfdYr1&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1JNEuMBQkj2WerZUXLGNXTbO-y-pTJ-Gs&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1sMf_vgsmQAJGpu6ih2EXNWw_aN1IyKUY&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1zDZJwIB4whY194Rj9OAVt_6dqmOp7pm8&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1bmo5nBykgPJOhap-7YWxjlUS5knljQuE&sz=w1080",
+          "https://drive.google.com/thumbnail?id=1Wqk8IPCkE2J8cYWPB9Ni2ENS3x7OyhgV&sz=w1080"
+          
+        ] 
+        };
+  },
+});
+
+app.mount('#app');
